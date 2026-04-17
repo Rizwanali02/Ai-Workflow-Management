4d340d87b58ae6aad9bf242905db0cf1d3c466af
Rizwan Ali <rizwanali02hindoli@gmail.com>
2026-04-17 17:57:32 +0530

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit tempore nostrum consequuntur voluptatum, sint libero veritatis neque optio, similique exercitationem molestiae deleniti repellat aliquid ab nisi error. Expedita, possimus temporibus inventore ab error sed odio quasi explicabo consectetur. Ab enim nobis consequuntur! Debitis dolor deleniti expedita blanditiis possimus labore, vitae sint consequuntur fugit corrupti. Expedita ullam recusandae impedit similique corporis quasi. Ipsum animi dolorem commodi magnam ratione, nostrum ut, doloribus dolores facere in a quisquam nobis quo officia obcaecati? Quia placeat expedita tenetur aspernatur libero commodi blanditiis magni, debitis, dolores quasi quidem laboriosam, corrupti quae modi temporibus. Odio, cum qui.
+    </div>
+  );
+}
